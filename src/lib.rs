6c422ce97603127e2fd54ad3da12ddf611bf16f7
VPR <malikrb@live.com>
2023-04-17 15:46:41 -0400

    improved src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -16,23 +16,21 @@ use winapi::shared::minwindef::{
 #[no_mangle]
 #[allow(non_snake_case, unused_variables)]
 
-extern "system" fn DllMain(h_instance: HINSTANCE, dw_reason: DWORD, _lp_reserved: LPVOID) -> BOOL {
+unsafe extern "system" fn DllMain(h_instance: HINSTANCE, dw_reason: DWORD, _lp_reserved: LPVOID) -> BOOL {
     const DLL_PROCESS_DETACH: DWORD = 0;
     const DLL_PROCESS_ATTACH: DWORD = 1;
 
     match dw_reason {
         DLL_PROCESS_ATTACH => {
-            unsafe {
-                libloaderapi::DisableThreadLibraryCalls(h_instance);
-                processthreadsapi::CreateThread(
-                    std::ptr::null_mut(),
-                    0,
-                    Some(main_thread),
-                    h_instance as LPVOID,
-                    0,
-                    std::ptr::null_mut()
-                );
-            }
+            libloaderapi::DisableThreadLibraryCalls(h_instance);
+            processthreadsapi::CreateThread(
+                std::ptr::null_mut(),
+                0,
+                Some(main_thread),
+                h_instance as LPVOID,
+                0,
+                std::ptr::null_mut()
+            );
         },
         DLL_PROCESS_DETACH => (),
         _ => ()
@@ -41,7 +39,7 @@ extern "system" fn DllMain(h_instance: HINSTANCE, dw_reason: DWORD, _lp_reserved
     minwindef::TRUE
 }
 
-unsafe extern "system" fn main_thread(h_instance: *mut winapi::ctypes::c_void) -> u32 {
+unsafe extern "system" fn main_thread(h_instance: *mut winapi::ctypes::c_void) -> DWORD {
     libc::printf("%d\n\0".as_ptr() as *const _, 42069);
     libloaderapi::FreeLibraryAndExitThread(h_instance as HMODULE, 0);
 
